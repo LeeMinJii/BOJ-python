757161106fb70b03e7bf8961ac186e9fe65e9cac
MinjiLee <julia7450@naver.com>
2025-04-07 00:44:15 +0900

    boj/python

---
+++ 1000.py
@@ -0,0 +1,17 @@
+# A + B
+
+#1
+# a, b = input().split()
+# a, b = int(a), int(b)
+# print(a+b)
+
+#2
+# a, b = map(int, input().split())
+# print(a+b)
+
+#3
+# numbers = list(map(int, input().split()))
+# print(sum(numbers))
+
+#4
+print(sum(list(map(int, input().split()))))
